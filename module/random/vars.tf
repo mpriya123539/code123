629ced7bfe15dbea7244f4dcfbefb679827e023c
mpriya123539 <77589263+mpriya123539@users.noreply.github.com>
2021-03-09 09:14:54 -0500

    Delete module directory

--- module/random/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "resourcegroupname" {
-  type    = string
-      default = "cdashub"
-}
-
-variable "byte_length" {
-  type    = number
-}
-
--- module/routetablesubnetassociation/main.tf
+++
@@ -1,9 +0,0 @@
-
-resource "azurerm_subnet_route_table_association" "routetableassociation" {
-  subnet_id      = var.subnetid
-  route_table_id = var.routetableid
-  depends_on = [var.routedependson]
-}
-output "id" {
-    value = azurerm_subnet_route_table_association.routetableassociation.id
-} 
